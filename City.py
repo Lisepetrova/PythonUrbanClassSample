8b3352ab065ec0760354eaa43b74136d705a0028
Lisepetrova <115191944+Lisepetrova@users.noreply.github.com>
2022-10-07 15:12:53 +0300

    Create City.py

---
+++ City.py
@@ -0,0 +1,17 @@
+class City: 
+
+  def __init__(self, name: str, population:int):
+
+   self.name = name
+   self.population = population
+   
+  def get_city_info (self):
+   return "Город - {self.name}. Население:{self.population}"
+   
+
+  city_1 = City ("Piterburg", 5377503)
+  city_2 = City ("Vladimir", 354752)
+
+print (city_1.name)
+print (city_1.get_city_info())
+print (city_2.get_city_info())
